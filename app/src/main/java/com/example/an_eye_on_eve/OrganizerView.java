bdd7ef76411fb39fbe0659dbc70e796dd839096f
Reethureddy28 <reethureddy2000@gmail.com>
2022-10-08 22:59:44 -0500

    Create OrganizerView.java

---
+++ app/src/main/java/com/example/an_eye_on_eve/OrganizerView.java
@@ -0,0 +1,14 @@
+package com.example.an_eye_on_eve;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizer_view);
+    }
+}
