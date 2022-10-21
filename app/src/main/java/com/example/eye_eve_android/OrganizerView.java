d7a5de49f0aaf1601f992a7b70f6dcc84cde7ff0
DwithiPathi <98133303+DwithiPathi@users.noreply.github.com>
2022-10-21 17:29:28 -0500

    Implemented menu bar for the app and added fragments

--- app/src/main/java/com/example/eye_eve_android/OrganizerView.java
+++ app/src/main/java/com/example/eye_eve_android/OrganizerView.java
@@ -4,7 +4,13 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,8 +18,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class OrganizerView extends AppCompatActivity {
 
@@ -34,6 +44,46 @@ public class OrganizerView extends AppCompatActivity {
         EventRecycler.setAdapter(EventServer);
         LinearLayoutManager myManager = new LinearLayoutManager(this);
         EventRecycler.setLayoutManager(myManager);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+
+                int id = item.getItemId();
+                item.setChecked(true);
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id)
+                {
+
+                    case R.id.nav_home:
+                        replaceFragment(new HomeFragment());break;
+                    case R.id.nav_CreateAnEvent:
+                        replaceFragment(new AddBoxFragment());break;
+                    case R.id.nav_organizer:
+                        replaceFragment(new ChecklistFragment());break;
+                    case R.id.settings:
+                        replaceFragment(new SettingsFragment());break;
+                    case R.id.nav_logout:
+                        replaceFragment(new LoginFragment());break;
+                    case R.id.about:
+                        replaceFragment(new Emoji_PeopleFragment());break;
+                    default:
+                        return true;
+
+                }
+                return true;
+            }
+        });
     }
 
     public void onClickCreate(View v){
@@ -58,6 +108,16 @@ public class OrganizerView extends AppCompatActivity {
                 }
             });
 
+
+
+
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
 //    public void oncreatenewEveOV(String eveName,String eveLoc, String eveDate, String eveTime){
 //        Log.v("in ov","before notify");
 //     myModel.eventList.add(new EventModel.Event(eveName,eveLoc,eveDate,eveTime));
--- app/src/main/java/com/example/eye_eve_android/UserView.java
+++ app/src/main/java/com/example/eye_eve_android/UserView.java
@@ -1,13 +1,23 @@
 package com.example.eye_eve_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class UserView extends AppCompatActivity {
     private EventModel myModel;
@@ -23,12 +33,58 @@ public class UserView extends AppCompatActivity {
         EventRecycler.setAdapter(EventServer);
         LinearLayoutManager myManager = new LinearLayoutManager(this);
         EventRecycler.setLayoutManager(myManager);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+
+                int id = item.getItemId();
+                item.setChecked(true);
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id)
+                {
+
+                    case R.id.nav_home:
+                        replaceFragment(new HomeFragment());break;
+                    case R.id.nav_organizer:
+                        replaceFragment(new ChecklistFragment());break;
+                    case R.id.nav_logout:
+                        replaceFragment(new LoginFragment());break;
+                    case R.id.about:
+                        replaceFragment(new Emoji_PeopleFragment());break;
+                    default:
+                        return true;
+
+                }
+                return true;
+            }
+        });
     }
     public void oncreatenewEveUV(String eveName,String eveLoc, String eveDate, String eveTime){
         Log.v("in uv","before notify");
         myModel.eventList.add(new EventModel.Event(eveName,eveLoc,eveDate,eveTime));
         EventServer.notifyItemInserted(myModel.eventList.size()-1);
         Log.v("in ov","after notify");
+    }
+
+
+
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 //    public void onClickcard(View v){
 //        Intent i=new Intent(this,EventDetailView.class);
