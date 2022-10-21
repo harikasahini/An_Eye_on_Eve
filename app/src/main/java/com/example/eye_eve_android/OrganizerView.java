f5e61b5a2c4e220a59b1974bc05e2fc378203109
Harika Sahini <s546653@nwmissouri.edu>
2022-10-20 23:19:11 -0500

    Implemented recycler view

--- app/src/main/java/com/example/eye_eve_android/OrganizerView.java
+++ app/src/main/java/com/example/eye_eve_android/OrganizerView.java
@@ -1,6 +1,8 @@
 package com.example.eye_eve_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,10 +10,19 @@ import android.view.View;
 
 public class OrganizerView extends AppCompatActivity {
 
+    private EventModel myModel;
+    private RecyclerView TaskRecycler;
+    private EventAdapter taskServer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer_view);
+        myModel= EventModel.getSingleton();
+        taskServer = new EventAdapter(myModel);
+        TaskRecycler = findViewById(R.id.tasksRV);
+        TaskRecycler.setAdapter(taskServer);
+        LinearLayoutManager myManager = new LinearLayoutManager(this);
+        TaskRecycler.setLayoutManager(myManager);
     }
 
     public void onClickCreate(View v){
--- app/src/main/java/com/example/eye_eve_android/UserView.java
+++ app/src/main/java/com/example/eye_eve_android/UserView.java
@@ -1,21 +1,31 @@
 package com.example.eye_eve_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class UserView extends AppCompatActivity {
-
+    private EventModel myModel;
+    private RecyclerView TaskRecycler;
+    private EventAdapter taskServer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_view);
+        myModel= EventModel.getSingleton();
+        taskServer = new EventAdapter(myModel);
+        TaskRecycler = findViewById(R.id.tasksRV);
+        TaskRecycler.setAdapter(taskServer);
+        LinearLayoutManager myManager = new LinearLayoutManager(this);
+        TaskRecycler.setLayoutManager(myManager);
     }
 
-    public void onClickcard(View v){
-        Intent i=new Intent(this,EventDetailView.class);
-        startActivity(i);
-    }
+//    public void onClickcard(View v){
+//        Intent i=new Intent(this,EventDetailView.class);
+//        startActivity(i);
+//    }
 }
