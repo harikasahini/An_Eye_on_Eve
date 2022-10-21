969d0b0d5256e2b6d060ad0fbe0716ca7aec3840
Harika Sahini <s546653@nwmissouri.edu>
2022-10-21 17:37:39 -0500

    Added event detail view navigation from each short card

--- app/src/main/java/com/example/eye_eve_android/EventDetailView.java
+++ app/src/main/java/com/example/eye_eve_android/EventDetailView.java
@@ -2,13 +2,46 @@ package com.example.eye_eve_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EventDetailView extends AppCompatActivity {
+
+    private String eveName;
+    private String eveLoc;
+    private String eveDesc;
+    private String eveDate;
+    private String eveTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail_view);
+
+        Intent intent=getIntent();
+        this.eveName = intent.getExtras().getString("EventName");
+        this.eveLoc = intent.getExtras().getString("EventLoc");
+        this.eveDate = intent.getExtras().getString("EveDate");
+        this.eveDesc = intent.getExtras().getString("description");
+        this.eveTime = intent.getExtras().getString("EveTime");
+
+
+        TextView eveNameTV=findViewById(R.id.eveTitleTV);
+        eveNameTV.setText(eveName);
+
+        TextView eveLocTV=findViewById(R.id.eveLocTV);
+        eveLocTV.setText("Location : "+eveLoc);
+
+        TextView eveDateTV=findViewById(R.id.eveDateTV);
+        eveDateTV.setText("Date : "+eveDate);
+
+        TextView eveDescTV=findViewById(R.id.eveDecTV);
+        eveDescTV.setText("Description : "+eveDesc);
+
+        TextView eveTimeTV=findViewById(R.id.eveTimeTV);
+        eveTimeTV.setText("Time : "+eveTime);
+
+
     }
 }
--- app/src/main/java/com/example/eye_eve_android/EventModel.java
+++ app/src/main/java/com/example/eye_eve_android/EventModel.java
@@ -10,12 +10,22 @@ public class EventModel {
         public String eventLocation;
         public String date;
         public String time;
+        public String eventDescription;
 
-        public Event(String eventName, String eventLocation, String date, String time) {
+        public Event(String eventName, String eventLocation, String date, String time,String eventDescription) {
             this.eventName=eventName;
             this.eventLocation=eventLocation;
             this.date=date;
             this.time=time;
+            this.eventDescription=eventDescription;
+        }
+
+        public String getEventDescription() {
+            return eventDescription;
+        }
+
+        public void setEventDescription(String eventDescription) {
+            this.eventDescription = eventDescription;
         }
 
         public String getEventName() {
@@ -64,15 +74,15 @@ public class EventModel {
         loadInitialTasks();
     }
     private void loadInitialTasks() {
-        Event t1 = new Event("T&T", "Maryville","2022-09-20","12:00 PM");
+        Event t1 = new Event("T&T", "Maryville","2022-09-20","12:00 PM", "Its a train and track event for children who learns art");
         eventList.add(t1);
-        Event t2 = new Event("A&A", "Jincoki","2022-09-27","1:00 PM");
+        Event t2 = new Event("A&A", "Jincoki","2022-09-27","1:00 PM","Its a concert");
         eventList.add(t2);
-        Event t3 = new Event("Wincoson trailers", "Missouri","2022-09-01","10:00 PM");
+        Event t3 = new Event("Wincoson trailers", "Missouri","2022-09-01","10:00 PM","It was a good playful event");
         eventList.add(t3);
-        Event t4 = new Event("Icognito treats", "Dallas","2022-12-06","9:00 PM");
+        Event t4 = new Event("Icognito treats", "Dallas","2022-12-06","9:00 PM","It was fun to see you here");
         eventList.add(t4);
-        Event t5 = new Event("Increaser chance of views", "Los Angeles","2022-09-12","9:00 PM");
+        Event t5 = new Event("Increaser chance of views", "Los Angeles","2022-09-12","9:00 PM","Play and learn more and more");
         eventList.add(t5);
     }
 
