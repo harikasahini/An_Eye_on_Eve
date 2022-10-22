66356c50268970ac8b506fda1ee87aca54c43465
Harika Sahini <s546653@nwmissouri.edu>
2022-10-21 22:01:25 -0500

    Added event details

--- app/src/main/java/com/example/eye_eve_android/EventModel.java
+++ app/src/main/java/com/example/eye_eve_android/EventModel.java
@@ -74,15 +74,15 @@ public class EventModel {
         loadInitialTasks();
     }
     private void loadInitialTasks() {
-        Event t1 = new Event("T&T", "Maryville","2022-09-20","12:00 PM", "Its a train and track event for children who learns art");
+        Event t1 = new Event("Brick-or-Treat Monster Party!", "Maryville","2022-11-09","12:00 PM", "Join the Monsters as they take over LEGOLAND® Discovery Center Chicago to celebrate the release of their brand-new Halloween 4D Movie, 'The Great Monster Chase!'.");
         eventList.add(t1);
-        Event t2 = new Event("A&A", "Jincoki","2022-09-27","1:00 PM","Its a concert");
+        Event t2 = new Event("Seedfolks", "Kansas city","2022-10-22","1:00 PM","A vacant lot in a broken neighborhood in the middle of the city can become a lot of things. A garbage dump. A gathering spot for trouble.");
         eventList.add(t2);
-        Event t3 = new Event("Wincoson trailers", "Missouri","2022-09-01","10:00 PM","It was a good playful event");
+        Event t3 = new Event("Vintage Garage Brings Vintage to The LOT in Highland Park", "California","2022-10-23","10:00 PM","Savoring the end of outdoor event season, a Vintage Market is coming to the City of Highland Park's alfresco entertainment space The Lot (523 Central) in downtown Highland Park on Sunday.");
         eventList.add(t3);
-        Event t4 = new Event("Icognito treats", "Dallas","2022-12-06","9:00 PM","It was fun to see you here");
+        Event t4 = new Event("Antique Bottle Show and Sale", "Dallas","2022-12-06","9:00 PM","Dealers across two floors of Macungie Park Hall selling quality antique bottles of all kinds and small table-top antiques. Dairy, breweriana, inks, poisons, stoneware, insulators, soda water, art deco soda, fruit and food jars, much more.");
         eventList.add(t4);
-        Event t5 = new Event("Increaser chance of views", "Los Angeles","2022-09-12","9:00 PM","Play and learn more and more");
+        Event t5 = new Event("Sarasota Water Lantern Festival", "Los Angeles","2022-10-29","9:00 PM","Water Lantern Festival is a floating lantern event that is all about connections. Magical nights in cities across the U.S. include food, games, activities, vendors, music and the beauty of thousands of lanterns adorned with letters of love, hope and dreams reflected upon the water.");
         eventList.add(t5);
     }
 
